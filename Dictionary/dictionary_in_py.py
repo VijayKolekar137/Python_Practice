2e6e045f9f9770d4730421f0d33e33da0a5c234d
vijay <vijaykolekar137@gmail.com>
2022-06-15 23:04:16 +0530

    if_statement and Dictionary

---
+++ Dictionary/dictionary_in_py.py
@@ -0,0 +1,48 @@
+# Simple Dictionary
+
+alien_0 = {'color' : 'green', 'points' : 5}
+
+print(alien_0['color'])
+print(alien_0['points'])
+
+# Working with dictionaries
+
+new_points = alien_0['points']
+
+print(f"You just earned {new_points} points!")
+
+# Adding new value pair in dictionary
+
+print("\nAdding new value pair in dictionary\n")
+
+print(alien_0)
+
+alien_0['x_position'] = 0
+alien_0['y_position'] = 25
+
+print(alien_0)
+
+# Adding element in empty list
+
+print('\nAdding element in empty list\n')
+
+em_li = {}
+
+em_li['color'] = 'red'
+em_li['points'] = 15
+
+print(em_li)
+
+# Modifying values in dictionary
+
+print('Modifying values in dictionary')
+
+print('\nPrevious value of dictionary\n')
+alien_0 = {'color' : 'green'}
+print(alien_0)
+
+print('\nNew value of dictionary\n')
+alien_0['color'] = 'yellow'
+print(alien_0)
+
+
--- If_Statement/if_statement.py
+++ If_Statement/if_statement.py
@@ -7,3 +7,106 @@ for c in cars:
 		print(c.upper())
 	else:
 		print(c.title())
+
+
+# Conditional Tests
+
+print('\n Checking for equality')
+
+car = 'bmw'
+
+print(car == 'bmw')
+
+car = 'Audi'
+
+print(car == 'bmw')
+
+print(car.lower() == 'audi')
+
+print(car)
+
+
+# Checking for inequality
+
+print('\nChecking for inequality')
+
+req_top = 'mushrooms'
+
+if req_top == 'anchovies':
+	print('Hold the achovies!')
+else:
+	print('Hold the mushrooms')
+
+
+# Numerical Comparison
+
+print('\nNumerical Comparison')
+
+age = 18
+
+print(age == 18)
+
+print('\nExample 2')
+
+ans = 17
+
+if ans != 42:
+	print('That is not the correct answer, please try again!')
+
+# Checking wheter a value is not in a list
+
+print('\nChecking wheter a value is not in a list\n')
+
+ban_users = ['andrew','carolina','david']
+user = 'marie'
+
+print(ban_users)
+
+if user not in ban_users:
+	print(f"{user.title()}, you can post a response if you wish.")
+
+# if-else statement:
+
+print('\n if-else statement\n')
+
+age = 18
+
+if age >= 18:
+	print('You are old enough to vote!\nHave you registered to vote yet?')
+else:
+	print('Sorry, you are too young to vote')
+
+# Checking for special items
+
+print('\nChecking for special items\n')
+
+req_top = ['mashrooms','green peppers', 'extra cheese']
+
+for rp in req_top:
+	print(f"Adding {rp}.")
+
+print('\nFinised making your pizza!')
+
+# Using multiple lists
+
+print('\nUsing multiple lists\n')
+
+avi_top = ['mushrooms','olives','green pepprs',
+	'pepperoni', 'pineapple','extra chhese']
+
+req_top = ['mushrooms','french fires','extra cheese']
+
+print(req_top)
+
+for rq in req_top:
+	if rq in avi_top:
+		print(f"Adding {rq}.")
+	else:
+		print(f"Sorry, we don't have {rq}.")
+
+print("\nFinised making your pizza!\n")
+
+
+ 
+
+
